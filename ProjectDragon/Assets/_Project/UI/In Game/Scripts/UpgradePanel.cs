33f5e232d1da4aba72054337c2faeb69cd13e947
LPAde <andre-peters01@hotmail.de>
2021-10-07 11:32:23 +0200

    Updated Unit Panel
    Upgrades the Unit Panel so you now can use the buttons to enable skills

--- ProjectDragon/Assets/_Project/UI/In Game/Scripts/UpgradePanel.cs
+++ ProjectDragon/Assets/_Project/UI/In Game/Scripts/UpgradePanel.cs
@@ -1,6 +1,4 @@
-using _Project.GamePlay.Player.Commander.BaseCommanderClass.Scripts;
 using _Project.SkillSystem.SkillTree;
-using _Project.Units.Unit.BaseUnits;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,31 +6,68 @@ namespace _Project.UI.In_Game.Scripts
 {
     public class UpgradePanel : MonoBehaviour
     {
-        [SerializeField] private Button[] skills;
         [SerializeField] private Image[] skillImages;
         [SerializeField] private Sprite a;
 
         private SkillTree _skillTree;
-        
+
+        #region Public Methods
+
+        /// <summary>
+        /// Upgrades the skill tree.
+        /// </summary>
+        /// <param name="skillTree"></param>
         public void UpdateSkillTree(SkillTree skillTree)
         {
             _skillTree = skillTree;
-            InitializeInfos();
+            UpdateImages();
         }
 
-        private void InitializeInfos()
+        /// <summary>
+        /// Upgrades the skill if possible.
+        /// </summary>
+        /// <param name="btn"></param>
+        public void OnClick(Button btn)
+        {
+            string buttonName = btn.gameObject.name;
+            
+            if(!_skillTree.tree[buttonName].IsLearnable)
+                return;
+            
+            _skillTree.tree[buttonName].EnableSkill();
+            UpdateImages();
+        }
+
+        #endregion
+
+
+        #region Private Methods
+
+        /// <summary>
+        /// Updates the all Images. 
+        /// </summary>
+        private void UpdateImages()
         {
             for (int i = 0; i < skillImages.Length; i++)
             {
                 string key = (i + 1).ToString();
-                
+
+                // Fail check.
                 if (_skillTree.tree[key].Sprite == null)
                 {
                     skillImages[i].sprite = a;
                     continue;
                 }
+
+                // Update the sprite.
                 skillImages[i].sprite = _skillTree.tree[key].Sprite;
+
+                // Make the image grey when the skill was neither learned nor is learnable.
+                if (!_skillTree.tree[key].IsLearnable || !_skillTree.tree[key].IsSkillActive)
+                    skillImages[i].color = Color.gray;
             }
         }
+
+        #endregion
     }
 }
