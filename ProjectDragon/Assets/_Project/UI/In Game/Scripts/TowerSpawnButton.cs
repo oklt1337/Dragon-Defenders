6ae84b483f35b1be7d78d17f90cc51cbccab173a
LPAde <andre-peters01@hotmail.de>
2021-10-12 23:34:10 +0200

    Tower Spawning
    Units can now be spawned with drag and drop buttons

---
+++ ProjectDragon/Assets/_Project/UI/In Game/Scripts/TowerSpawnButton.cs
@@ -0,0 +1,58 @@
+using _Project.GamePlay.GameManager.Scripts;
+using _Project.Units.Unit.BaseUnits;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _Project.UI.In_Game.Scripts
+{
+    public class TowerSpawnButton : MonoBehaviourPun, IPointerClickHandler, IBeginDragHandler, IDragHandler,
+        IEndDragHandler
+    {
+        [SerializeField] private Unit unit;
+        [SerializeField] private Camera buildCam;
+
+        #region Interface Methods
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            //TODO: Instantiate transparent unit. (Not Photon Instantiate!)
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            //TODO: Move transparent unit with courser.
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            if (GameManager.Instance.PlayerModel.Money < unit.Cost)
+                return;
+
+            Vector3 screenPos = Input.mousePosition;
+            screenPos.z = buildCam.nearClipPlane;
+            Ray ray = buildCam.ScreenPointToRay(screenPos);
+
+
+            Physics.Raycast(ray, out RaycastHit hit);
+
+            if (!hit.collider.CompareTag("Ground")) 
+                return;
+            
+            // Do the spawning when everything works out.
+            PhotonNetwork.Instantiate(string.Concat("Units/", unit.name), hit.point, Quaternion.identity);
+            GameManager.Instance.PlayerModel.AddMoney(-unit.Cost);
+        }
+
+        #endregion
+
+        public void UpdateUnitName(Unit newUnit)
+        {
+            unit = newUnit;
+        }
+    }
+}
