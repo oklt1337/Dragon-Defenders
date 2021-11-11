d52669f54decb6d0346cce8d264ad1394b3752e6
Christopher Zelch <71003691+oklt1337@users.noreply.github.com>
2021-11-11 03:55:36 +0100

    Addes some filter

--- ProjectDragon/Assets/_Project/Deck&Cards/Utility/Scripts/CardUtil.cs
+++ ProjectDragon/Assets/_Project/Deck&Cards/Utility/Scripts/CardUtil.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections.Generic;
-using Deck_Cards.Cards.CommanderCard.Scripts;
+using Deck_Cards.Cards.BaseCards.Scripts;
 using Deck_Cards.Cards.UnitCard.Scripts;
+using Faction;
 
 namespace Deck_Cards.Utility.Scripts
 {
     public class CardUtil
     {
-        public static List<UnitCard> SearchUnitsByName(string cardName, List<UnitCard> unitCards)
+        #region Search
+
+        public static List<BaseCard> SearchByName(string cardName, List<BaseCard> cards)
         {
-            var matches = new List<UnitCard>();
-            unitCards.ForEach(card =>
+            var matches = new List<BaseCard>();
+            cards.ForEach(card =>
             {
                 var startsWith = card.cardName.StartsWith(cardName);
                 if (startsWith)
@@ -19,31 +22,56 @@ namespace Deck_Cards.Utility.Scripts
             if (matches.Count != 0) 
                 return matches;
             {
-                var index = unitCards.FindIndex(card => card.CardName == cardName);
+                var index = cards.FindIndex(card => card.CardName == cardName);
                 if (index != -1) 
-                    matches.Add(unitCards[index]);
+                    matches.Add(cards[index]);
             }
+            return matches;
+        }
+
+        #endregion
+
+        #region Filter
+
+        public static List<BaseCard> FilterCommandersByFaction(ClassAndFaction.Faction faction, List<BaseCard> cards)
+        {
+            var matches = new List<BaseCard>();
+            cards.ForEach(card =>
+            {
+                if (card.Faction == faction)
+                {
+                    matches.Add(card);
+                }
+            });
             return matches;
         }
         
-        public static List<CommanderCard> SearchCommandersByName(string cardName, List<CommanderCard> commanderCards)
+        public static List<BaseCard> FilterCommandersByClass(ClassAndFaction.Class cClass, List<BaseCard> cards)
         {
-            var matches = new List<CommanderCard>();
-            commanderCards.ForEach(card =>
+            var matches = new List<BaseCard>();
+            cards.ForEach(card =>
             {
-                var startsWith = card.cardName.StartsWith(cardName);
-                if (startsWith)
+                if (card.Class == cClass)
+                {
                     matches.Add(card);
+                }
             });
-
-            if (matches.Count != 0) 
-                return matches;
-            {
-                var index = commanderCards.FindIndex(card => card.CardName == cardName);
-                if (index != -1)
-                    matches.Add(commanderCards[index]);
-            }
             return matches;
         }
+        
+        public static List<BaseCard> FilterCommandersByRarity(List<Rarity> rarities, List<BaseCard> cards)
+        {
+            var matches = new List<BaseCard>();
+            cards.ForEach(card =>
+            {
+                if (rarities.Contains(card.Rarity))
+                {
+                    matches.Add(card);
+                }
+            });
+            return matches;
+        }
+
+        #endregion
     }
 }
