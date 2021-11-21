aafb9b99b5b250ec25b2e2e7540014dc0e2bda25
Christopher Zelch <71003691+oklt1337@users.noreply.github.com>
2021-11-21 12:39:56 +0100

    Troubling truculent trolls skilltrree done

---
+++ ProjectDragon/Assets/_Project/Abilities/Effects/DecreaseDamageEveryXAttackEffect/Scripts/DecreaseDamageEveryXAttackEffect.cs
@@ -0,0 +1,37 @@
+﻿using Abilities.Ability.Scripts;
+using UnityEngine;
+
+namespace Abilities.Effects.DecreaseDamageEveryXAttackEffect.Scripts
+{
+    public class DecreaseDamageEveryXAttackEffect : MonoBehaviour
+    {
+        private int counter;
+        private int casted;
+        private float modifier;
+        private DamageAbility damageAbility;
+        private float damage;
+
+        public void Init(float value, int xTime, DamageAbility ability)
+        {
+            damageAbility = ability;
+            modifier = value;
+            counter = xTime;
+            damageAbility.Casted += (t) => Counter();
+            damage = ability.Damage;
+        }
+
+        private void Counter()
+        {
+            casted++;
+            if (casted == counter - 1)
+            {
+                damageAbility.Damage *= modifier;
+            }
+            else if (casted == counter)
+            {
+                casted = 0;
+                damageAbility.Damage = damage;
+            }
+        }
+    }
+}
---
+++ ProjectDragon/Assets/_Project/SkillSystem/Nodes/EffectNodes/Scripts/DecreaseDamageEveryXAttack.cs
@@ -0,0 +1,21 @@
+﻿using Abilities.Ability.Scripts;
+using Abilities.Effects.DecreaseDamageEveryXAttackEffect.Scripts;
+using Abilities.VisitorPattern.Scripts;
+using UnityEngine;
+
+namespace SkillSystem.Nodes.EffectNodes.Scripts
+{
+    [CreateAssetMenu(menuName = "AbilitySystem/SkillTree/Nodes/EffectNodes/DecreaseDamageEveryXAttack",
+        fileName = "DecreaseDamageEveryXAttack")]
+    public class DecreaseDamageEveryXAttack : BaseNodes.Scripts.EffectNodes
+    {
+        [SerializeField] private int xTime;
+        public override void Execute(IVisitor visitor)
+        {
+            if (!(visitor is DamageAbility ability))
+                return;
+            var effect = ability.Owner.gameObject.AddComponent<DecreaseDamageEveryXAttackEffect>();
+            effect.Init(value, xTime, ability);
+        }
+    }
+}
