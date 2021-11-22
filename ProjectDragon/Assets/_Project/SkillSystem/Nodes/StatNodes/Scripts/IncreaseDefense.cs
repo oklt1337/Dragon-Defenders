908552fdc84b63151de3cb170b304740f3352ce7
Christopher Zelch <71003691+oklt1337@users.noreply.github.com>
2021-11-22 18:35:34 +0100

    Changed naming of skills

---
+++ ProjectDragon/Assets/_Project/SkillSystem/Nodes/StatNodes/Scripts/IncreaseDefense.cs
@@ -0,0 +1,19 @@
+﻿using Abilities.VisitorPattern.Scripts;
+using GamePlay.Player.Commander.CommanderStats.Scripts;
+using SkillSystem.Nodes.BaseNodes.Scripts;
+using UnityEngine;
+
+namespace SkillSystem.Nodes.StatNodes.Scripts
+{
+    [CreateAssetMenu(menuName = "AbilitySystem/SkillTree/Nodes/StatNodes/IncreaseDefense", fileName = "IncreaseDefense")]
+    public class IncreaseDefense : StatNode
+    {
+        public override void Execute(IVisitor visitor)
+        {
+            if (visitor is CommanderStats stats)
+            {
+                stats.Defense *= value;
+            }
+        }
+    }
+}
