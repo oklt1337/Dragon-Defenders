80848988ee1ecf870ed5613ec6a7c652ec0f3c14
Christopher Zelch <71003691+oklt1337@users.noreply.github.com>
2021-11-22 20:52:46 +0100

    Rackl skilltree done

---
+++ ProjectDragon/Assets/_Project/Abilities/Effects/BuffAllUnitsInRangeCooldownEffect/Scripts/BuffAllUnitsInRangeCooldownEffect.cs
@@ -0,0 +1,48 @@
+﻿using Abilities.Ability.Scripts;
+using GamePlay.Player.Commander.BaseCommanderClass.Scripts;
+using Units.Unit.BaseUnits;
+using UnityEngine;
+
+namespace Abilities.Effects.BuffAllUnitsInRangeCooldownEffect.Scripts
+{
+    public class BuffAllUnitsInRangeCooldownEffect : MonoBehaviour
+    {
+        [SerializeField] private SphereCollider sphereCollider;
+        
+        private float percentage;
+
+        public void Init(float value, float range)
+        {
+            sphereCollider.radius = range;
+            percentage = value;
+        }
+
+        private void ReduceCooldown(Unit unit)
+        {
+            if (unit == null)
+                return;
+            unit.Ability.CoolDown *= percentage;
+        }
+
+        private void ResetCooldown(Unit unit)
+        {
+            if (unit == null)
+                return;
+            unit.Ability.CoolDown /= percentage;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Unit")) 
+                return;
+            ReduceCooldown(other.GetComponent<Unit>());
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!other.CompareTag("Unit"))
+                return;
+            ResetCooldown(other.GetComponent<Unit>());
+        }
+    }
+}
---
+++ ProjectDragon/Assets/_Project/SkillSystem/Nodes/EffectNodes/Scripts/BuffAllUnitsInRange.cs
@@ -0,0 +1,33 @@
+﻿using Abilities.Ability.Scripts;
+using Abilities.Effects.BuffAllUnitsInRangeCooldownEffect.Scripts;
+using Abilities.Effects.IncreaseAttackDamageForXUnitsInXRangeEffect.Scripts;
+using Abilities.VisitorPattern.Scripts;
+using GamePlay.Player.Commander.BaseCommanderClass.Scripts;
+using UnityEngine;
+
+namespace SkillSystem.Nodes.EffectNodes.Scripts
+{
+    [CreateAssetMenu(menuName = "AbilitySystem/SkillTree/Nodes/EffectNodes/BuffAllUnitsInRange",
+        fileName = "BuffAllUnitsInRange")]
+    public class BuffAllUnitsInRange : BaseNodes.Scripts.EffectNodes
+    {
+        [SerializeField] private float range;
+        [SerializeField] private GameObject prefab;
+
+        public override void Execute(IVisitor visitor)
+        {
+            if (!(visitor is Ability ability))
+                return;
+
+            var owner = ability.Owner.GetComponent<Commander>();
+            if (owner == null)
+                return;
+            var effect = ability.Owner.GetComponentInChildren<BuffAllUnitsInRangeCooldownEffect>();
+            if (effect != null)
+                return;
+            var obj = Instantiate(prefab, ability.Owner.position, Quaternion.identity, ability.Owner);
+            effect = obj.GetComponent<BuffAllUnitsInRangeCooldownEffect>();
+            effect.Init(value, range);
+        }
+    }
+}
